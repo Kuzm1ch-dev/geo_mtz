6aa6c86d704a4d2a3b00cee59cbc176b7cc7393d
Kuzm1ch-dev <steam.danya@yandex.ru>
2022-12-16 20:11:34 +0300

    refactor code

--- data_gen.py
+++ data_gen.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from matplotlib.pyplot import step 
 
 
-class generator:
+class data_generator:
     def __init__(self, w,h,step, cw,ch,v,p,x,y):
         self.step = step
         self.w = w 
@@ -18,25 +18,23 @@ class generator:
         self.p = p
         self.x = x
         self.y = y
-    def generate_file(self):
-        a = []
-
-
-        #Первые 2 строки хуй пойми что
+    def generate_data_file(self):
+        #Заполняем дефолтные зачения
+        array = [] 
         for i in range (2):
             row = []
             for j in range (self.h):
                 row.insert(0,500)
-            a.insert(len(a),row)
+            array.insert(len(array),row)
 
         
-        #Остальные self.h строки
+        #Остальные строки
 
         for i in range (self.w):
             row = []
             for j in range (self.h):
                 row.insert(len(row)-1,self.p)
-            a.insert(len(a),row)
+            array.insert(len(array),row)
 
         #Квадрат
 
@@ -45,12 +43,12 @@ class generator:
         for i in range (self.w):
             for j in range (self.h):
                 if (i >= (self.y-1) and i <  (self.y-1) + self.ch and i < self.h) and (j >=  (self.x-1) and j <  (self.x-1) + self.cw and j < self.w):
-                    a[i][j] = value
+                    array[i][j] = value
 
         f = open("Data.txt", "w")
         with f:
             f.write(f"{self.w} {self.h}"  + "\n")
-            for r in a:
+            for r in array:
                 f.write(' '.join(str(i) for i in r) + "\n")
         f.close()
         pass
